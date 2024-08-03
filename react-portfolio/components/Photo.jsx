688c2a1a8f0b8e6fecec08c47f135bb896608f64
EdRivera016 <160804229+EdRivera016@users.noreply.github.com>
2024-08-03 10:32:35 -0400

    finished

--- react-portfolio/components/Photo.jsx
+++ react-portfolio/components/Photo.jsx
@@ -21,14 +21,14 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[328px] h-[328px] xl:w-[438px] xl:h-[468px] mix-blend-lighten rounded-full overflow-hidden"
+          className="w-[200px] h-[280px] xl:w-[300px] xl:h-[350px] mix-blend-lighten rounded-full overflow-hidden"
         >
           <Image
             src="/assets/IMAGE.png"
             priority
             quality={100}
             fill
-            alt=""
+            alt="Profile Image"
             className="object-cover"
           />
         </motion.div>
